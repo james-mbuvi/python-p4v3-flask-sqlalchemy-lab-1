1688c99dc8d76000746369417253383bda8bfc48
[james-mbuvi] <[james.mbuvi@student.moringaschool.com]>
2024-04-03 18:33:53 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,15 +1,12 @@
 # server/app.py
-#!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,10 +14,28 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
+    return jsonify({'message': 'Flask SQLAlchemy Lab 1'})
 
-# Add views here
+
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify(earthquake.serialize()), 200
+    else:
+        return jsonify({'message': f'Earthquake {id} not found.'}), 404
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    if earthquakes:
+        return jsonify({
+            'count': len(earthquakes),
+            'quakes': [earthquake.serialize() for earthquake in earthquakes]
+        }), 200
+    else:
+        return jsonify({'count': 0, 'quakes': []}), 200
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -2,8 +2,34 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+class Earthquake(db.Model):
+    __tablename__ = 'earthquakes'
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def  serialize(self):
+        return {
+           "id": self.id,
+            "magnitude": self.magnitude,
+            "location": self.location,
+            "year": self.year}
+
+
+
+
+    def __repr__(self):
+     return f'<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>'
+
+
+
+
+
